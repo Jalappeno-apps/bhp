b4170d8bacff5fae10113bc9a092cf3a848a8bdc
archetype2142 <malhotraritwick2011@gmail.com>
2020-10-12 10:11:18 +0200

    Added smtp host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   # config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "45.79.251.190" }
+  config.action_mailer.default_url_options = { host: "bhpartykuly.pl/" }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SMTP_USERNAME'],
