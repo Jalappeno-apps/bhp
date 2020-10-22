c0584d11e053d9dcad9274373766e0887dce1589
archetype2142 <malhotraritwick2011@gmail.com>
2020-10-22 20:24:48 +0200

    fix

--- app/controllers/spree/payu_controller.rb
+++ app/controllers/spree/payu_controller.rb
@@ -24,10 +24,10 @@ module Spree
 
       if order.payment_state == "balance_due"
         redirect_to order_path(order), flash: { success: "Dziękujemy za zamówienie, czekamy na potwierdzenie płatności. Proszę sprawdzić skrzynkę email" }
-        PayuStatusWorker.perform_async(order.number)
-        PayuStatusWorker.perform_in(2.minutes, order.number)
-        PayuStatusWorker.perform_in(10.minutes, order.number)
-        PayuStatusWorker.perform_in(20.minutes, order.number)
+        ::PayuStatusWorker.perform_async(order.number)
+        ::PayuStatusWorker.perform_in(2.minutes, order.number)
+        ::PayuStatusWorker.perform_in(10.minutes, order.number)
+        ::PayuStatusWorker.perform_in(20.minutes, order.number)
 
       elsif order.payment_state == "paid"
         redirect_to order_path(order), flash: { success: "Dziękujemy za zamówienie, prosimy o sprawdzenie skrzynki email" }
