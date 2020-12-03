ef711e21749bf8d43eb5274c6881244a95988926
Alan Pieczonka <alanpieczonka@gmail.com>
2020-12-03 14:40:14 +0100

    Create /returns page

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
     get 'store_return', to: "payu#store_return"
 
     get "/terms-of-service", to: "tos#index", as: :tos
+    get "/returns", to: "returns#index", as: :returns
     get "/", to: "under_construction#index"
     get "home", to: "home#index"
   end
