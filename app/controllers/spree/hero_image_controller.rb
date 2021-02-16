23289df71689ff3275c439d539983ea923dc52e8
Sourav Goswami <souravgoswami@protonmail.com>
2021-02-16 15:40:51 +0530

    Error handling and toast messages

--- app/controllers/spree/hero_image_controller.rb
+++ app/controllers/spree/hero_image_controller.rb
@@ -1,3 +1,5 @@
+# Frozen_String_Literal: true
+
 module Spree
 	class HeroImageController < Spree::Admin::BaseController
 		before_action :set_hero_image, only: %i(view destroy)
@@ -17,21 +19,18 @@ module Spree
 
 		def create
 			@hero_image = Spree::HeroImage.create(hero_image_params)
+
 			if @hero_image.save
-				redirect_to hero_images_path
+				redirect_with_notice "New hero image added for home page!"
 			else
+				flash.notice = "Unable to save image"
 				render 'new'
 			end
 		end
 
 		def destroy
 			@hero_image.destroy
-
-			respond_to do |f|
-				f.html {
-					redirect_to  hero_images_path, notice: "Image was successfully removed"
-				}
-			end
+			redirect_with_notice "Image was successfully removed"
 		end
 
 		private
@@ -39,8 +38,23 @@ module Spree
 			params.require(:hero_image).permit(:image)
 		end
 
-		def set_hero_image
-			@hero_image = Spree::HeroImage.find(params[:id])
+		def redirect_with_notice(message)
+			flash.notice = message
+			redirect_to hero_images_path
 		end
-	end
-end
+
+		def set_hero_image
+			id = params[:id].to_i
+
+			begin
+				@hero_image = Spree::HeroImage.find(id)
+
+			rescue ActiveRecord::RecordNotFound
+				redirect_with_notice("Image of ID #{id} not found")
+
+			rescue StandardError
+				redirect_with_notice("Oops! An Unknown error occurred")
+			end			# Rescue block
+		end				# end of set_hero_image definition
+	end					# End of HeroImageController
+end 					# End of module Spree
